a47ec302f366dbc9c4314c8d946c5a2b9b792b53
Brainner Blanco <brainnerblanco@gmail.com>
2024-05-29 15:33:45 -0600

    second safe zone

---
+++ public/build/js/scroll.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const linksNavigation = document.querySelectorAll('.navigation-bar a');
+  const linksFooter = document.querySelectorAll('.footer a'); // Selecciona todos los enlaces dentro de .footer
+
+  // Combina ambas listas de enlaces en un solo array
+  const allLinks = [...linksNavigation, ...linksFooter];
+
+  allLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      // Evita el comportamiento por defecto del enlace
+      event.preventDefault();
+
+      // Obtiene el ID del elemento objetivo desde el atributo href del enlace
+      const targetId = link.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        // Desplazamiento suave hacia el elemento objetivo
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
