269a2cc682b278e486e73dbf7dff688e94a7bd4c
BRAINER BLANCO <brainnerblanco@gmail.com>
2024-06-03 17:58:05 -0700

    fix project cards

--- public/build/js/navbar.js
+++ public/build/js/navbar.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('header');
-    const originalOffsetTop = header.offsetTop;
-  
-    window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  const originalOffsetTop = header.offsetTop;
+
+  // Verificar si hay un estado almacenado en el localStorage
+  const isHeaderFixed = localStorage.getItem('headerFixed');
+  if (isHeaderFixed === 'true') {
+      header.classList.add('fixed');
+  }
+
+  window.addEventListener('scroll', function() {
       const scrollY = window.scrollY;
-  
+
       // Verificar si el scroll ha superado la posición original del header
       if (scrollY > originalOffsetTop) {
-        header.classList.add('fixed');
+          header.classList.add('fixed');
+          // Almacenar el estado en el localStorage
+          localStorage.setItem('headerFixed', 'true');
       } else {
-        header.classList.remove('fixed');
+          header.classList.remove('fixed');
+          // Almacenar el estado en el localStorage
+          localStorage.setItem('headerFixed', 'false');
       }
-    });
   });
-  
+});
